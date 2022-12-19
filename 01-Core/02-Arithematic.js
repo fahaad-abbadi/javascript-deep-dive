3504e5d90958caf60f89e8b212e7df0eb4675763
Fahaad Al Abbadi <50259848+fahaad-abbadi@users.noreply.github.com>
2022-12-19 11:24:27 +0530

    Create 02-Arithematic.js

---
+++ 01-Core/02-Arithematic.js
@@ -0,0 +1,36 @@
+//sum
+console.log(7 + 6);
+
+//diff
+console.log(2 - 9);
+
+//mul
+console.log(7 * 8);
+
+//div 
+console.log(6 / 2);
+
+//bodmas rule
+console.log(1 - 2 * 4 / 2 + 3 - 6);
+
+//parenthesis have high precedence
+console.log((1 - 2) * 4 / 2 + (3 - 6));
+
+//exponent
+console.log(6 ** 2);
+
+//remainder
+console.log(5 % 10);
+
+
+//incrementing and decrementing operator
+
+//incrementing
+let num1 = 10;
+num1++;
+console.log(num1);
+
+//decrementing
+let num2 = 10;
+num1--;
+console.log(num1);
