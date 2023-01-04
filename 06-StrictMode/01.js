4bff02e8e146681cdecd933305c60b55e2d669b3
Fahaad Al Abbadi <50259848+fahaad-abbadi@users.noreply.github.com>
2023-01-04 10:26:43 +0530

    Create 01.js

---
+++ 06-StrictMode/01.js
@@ -0,0 +1,9 @@
+// it makes sure that the code should be executed in 'strict mode'
+
+
+// generally we can declare variables in this fashion
+sample = 9;
+console.log(sample);
+
+var temp = "this is some javascript";
+console.log(temp)
